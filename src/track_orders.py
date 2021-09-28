f60a79ba302f91ba06ea5781e7292a68823b696b
Ruan Miguel <ruanmiguelps@gmail.com>
2021-09-28 19:31:14 -0300

    3 out of 7

--- src/track_orders.py
+++ src/track_orders.py
@@ -10,6 +10,19 @@ class TrackOrders:
         pass
 
     def get_most_ordered_dish_per_costumer(self, costumer):
+        my_dict = dict()
+        value = 0
+        for x in self.orders:
+            if x[0] == costumer:
+                if x[0] not in my_dict:
+                    my_dict[x[0]] = 1
+                else:
+                    my_dict[x[0]] += 1
+                if my_dict[x[0]] > value:
+                    value = my_dict[x[0]]
+                    dish = x[1]
+        return dish
+
         pass
 
     def get_never_ordered_per_costumer(self, costumer):
